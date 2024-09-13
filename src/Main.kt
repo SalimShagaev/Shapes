7a89361452ac19ba76fe38ee32b7a9addb16c525
Salim Shagaev <shagaev.workmail@gmail.com>
2024-09-13 13:01:58 +0300

    added 2 new printnlns

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,11 @@
 fun main(){
 
+    println("Start of program")
+
     val rectangleMy = Rectangle(5,33)
     val myCircle = Circle(5.0)
-    val secondCircle = Circle(10.0)
     val firstTriangle = Triangle(3.0,4.0,5.0)
+
+    println("End of program")
 
 }
