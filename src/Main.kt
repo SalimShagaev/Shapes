d5b5f4a76e537bb8df8e6bb12984659f446c866e
Salim Shagaev <shagaev.workmail@gmail.com>
2024-09-13 13:02:36 +0300

    removed first println, added new one at the end

--- src/Main.kt
+++ src/Main.kt
@@ -1,11 +1,10 @@
 fun main(){
 
-    println("Start of program")
 
     val rectangleMy = Rectangle(5,33)
     val myCircle = Circle(5.0)
     val firstTriangle = Triangle(3.0,4.0,5.0)
 
     println("End of program")
-
+    println("would u mind start it again ?")
 }
